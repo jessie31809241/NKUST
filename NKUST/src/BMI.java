0aba8e88708a79999de5d24ad2cfb3a3c59a9ed1
jessie31809241 <93572183+jessie31809241@users.noreply.github.com>
2021-11-16 15:57:49 +0800

    Add files via upload

---
+++ NKUST/src/BMI.java
@@ -0,0 +1,8 @@
+public class BMI {
+        public static void main(String[] args) {
+            float height = 1.69f;
+            int weight = 49;
+            System.out.println(weight/(height*height));
+        }
+    }
+}
---
+++ NKUST/src/For_loop.java
@@ -0,0 +1,7 @@
+public class For_loop {
+    public static void main(String[] arges){
+        for (int i = 0; i < 10; i++){
+            System.out.println(i);
+        }
+    }
+}
