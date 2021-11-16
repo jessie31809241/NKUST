cc7993cfef07a6a6c288f5208f096a715ab427bf
jessie31809241 <514553234ivy@gmail.com>
2021-11-16 14:24:35 +0800

    This is my first commit

--- bmi.java
+++ bmi.java
@@ -1,8 +1,2 @@
-public class bmi {
-    public static void main(String[] args) {
-        String height = "1.68";
-        String weight = "48";
-        System.out.println(weight/(height*height));
-
-    }
+package PACKAGE_NAME;public class bmi {
 }
